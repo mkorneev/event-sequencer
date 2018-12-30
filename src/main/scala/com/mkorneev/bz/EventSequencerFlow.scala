e71312c1830c23ecb785c9921c9adc5f4c36964b
Maxim Korneev <maxim@mkorneev.com>
2018-12-30 18:51:53 +0100

    Revised the event sequencing algorithm

---
+++ src/main/scala/com/mkorneev/bz/EventSequencerFlow.scala
@@ -0,0 +1,50 @@
+package com.mkorneev.bz
+
+import java.time.{Duration, LocalDateTime}
+
+import akka.stream.stage.{GraphStage, GraphStageLogic, InHandler, OutHandler}
+import akka.stream.{Attributes, FlowShape, Inlet, Outlet}
+import com.manonthegithub.bz.UserAuthEvent
+
+class EventSequencerFlow(window: Duration)
+  extends GraphStage[FlowShape[UserAuthEvent, (String, EventsSeq[String])]] {
+
+  private val in = Inlet[UserAuthEvent]("EventSequencerFlow.in")
+  private val out = Outlet[(String, EventsSeq[String])]("EventSequencerFlow.out")
+
+  def createLogic(inheritedAttributes: Attributes): GraphStageLogic = new GraphStageLogic(shape) with InHandler with OutHandler {
+
+    private val sequencer = new EventSequencer[String, String](window)
+
+    override def onPush(): Unit = {
+      val event = grab(in)
+
+      sequencer.add(event.date, event.ip, event.username)
+
+      // Get only auth sequences of more than 1 element
+      val closedSequences = sequencer.takeClosedSequences(event.date).filter(_._2.values.size > 1)
+
+      if (closedSequences.isEmpty) {
+        pull(in)
+      } else {
+        emitMultiple(out, closedSequences.toIterator)
+      }
+    }
+
+    override def onPull(): Unit = {
+      pull(in)
+    }
+
+    override def onUpstreamFinish(): Unit = {
+      val closedSequences = sequencer.takeClosedSequences(LocalDateTime.MAX).filter(_._2.values.size > 1)
+
+      if (isAvailable(out)) emitMultiple(out, closedSequences.toIterator)
+      completeStage()
+    }
+
+    setHandler(in, this)
+    setHandler(out, this)
+  }
+
+  override def shape: FlowShape[UserAuthEvent, (String, EventsSeq[String])] = FlowShape(in, out)
+}
---
+++ src/test/scala-2.11/com/mkorneev/bz/TimeSequencerTest.scala
@@ -0,0 +1,59 @@
+package com.mkorneev.bz
+
+import java.time.temporal.ChronoUnit
+import java.time.{Duration, LocalDateTime}
+
+import org.scalatest.{FunSuite, Matchers}
+
+
+class TimeSequencerTest extends FunSuite with Matchers {
+
+  type IP = String
+  type User = String
+
+  private val window = Duration.ofMinutes(1)
+  private val time = LocalDateTime.now()
+
+  test("testEmpty") {
+    val ts = new EventSequencer[IP, User](window)
+
+    ts.takeClosedSequences(time) should have size 0
+  }
+
+  test("testSingle") {
+    val ts = new EventSequencer[IP, User](window)
+    ts.add(time, "IP", "User")
+
+    ts.takeClosedSequences(time.plus(10, ChronoUnit.MINUTES)) should have size 1
+  }
+
+  test("testSortedAssert") {
+    val ts = new EventSequencer[IP, User](window)
+    ts.add(time, "IP", "User")
+
+    assertThrows[AssertionError] {
+      ts.add(time.minus(1, ChronoUnit.SECONDS), "IP", "User")
+    }
+  }
+
+  test("testWindow") {
+    val ts = new EventSequencer[IP, User](window)
+    ts.add(time, "IP", "User")
+    ts.add(time, "IP", "User")
+    ts.add(time, "IP", "User")
+    ts.add(time.plus(10, ChronoUnit.MINUTES), "IP", "User")
+    ts.add(time.plus(10, ChronoUnit.MINUTES), "IP", "User")
+
+    val result = ts.takeClosedSequences(time.plus(10, ChronoUnit.MINUTES))
+    result should have size 1
+    result.head._2.values should have size 3
+  }
+
+  test("testOpen") {
+    val ts = new EventSequencer[IP, User](window)
+    ts.add(time, "IP", "User")
+
+    ts.takeClosedSequences(time) should be(empty)
+  }
+
+}
